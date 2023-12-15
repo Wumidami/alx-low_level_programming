6fea27d41cc3d5c69c45bc5cf6cc028245936ca0
wumidami <wumidami@gmail.com>
2023-12-14 17:29:45 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,5 +19,5 @@ printf("Size of a long int: %lu byte(s)\n", sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
 printf("Size of a float: %lu byte(s)\n", sizeof(e));
 
-(return 0;)
+(return) 0;
 }
