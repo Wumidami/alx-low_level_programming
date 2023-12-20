2dd6d9159bc9fe9c06bc198c9bc390136e00e8a9
wumidami <wumidami@gmail.com>
2023-12-20 05:06:25 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,31 +2,31 @@
 
 
 /**
-  * main - prints the first 50 fib
-  * Return: (0)
+  * main - prints the first 52 fib
+  * Return: 0.
   */
 
 
 int main(void)
 {
-	int i;
-	long int fibonacci[50];
-	
-	fibonacci[0] = 1;
-	fibonacci[1] = 2;
-	printf("%ld,%ld, ", fibonacci[0], fibonacci[1]);
+	int i = 0;
+	long j = 1, k = 2;
 
-	for (i = 2; i < 50; i++)
+
+	while (i < 50)
 	{
-		fibanacci[i] = fibonacci[i - 1] + fibonacci[i -2];
-		if (i ==49)
-		{
-			printf("%ld\n", fibonacci[i]);
-		}
-		else
-		{
-			printf("%ld, 'I', fibonacci[i]");
-		}
+	if (i == 0)
+	printf("%ld", j);
+	else if (i == 1)
+	printf(", %ld", k);
+	else
+	{
+	k += j;
+	j = k - j;
+	printf(", %ld", k);
 	}
-	return (0)
+	++i;
+	}
+	printf("\n");
+	return (0);
 }
